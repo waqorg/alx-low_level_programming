68d26c343a4ade385c184d4b0ff905308b271f90
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-14 08:27:13 +0000

    a C program that prints exactly with proper grammar but the outcome is a piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * use function printf
+ * You are not allowed to use the function puts
+ * Your program should return 0
+ * Your program should compile without warning when using the -Wall gcc option
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
