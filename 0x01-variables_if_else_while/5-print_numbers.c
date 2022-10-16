c93255a90b7842de5c4a1721d7b65b41905fa619
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-16 15:52:13 +0000

    a program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main-Entry
+ *
+ * Description:program prints single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int dig = 0;
+
+	for (dig = 0; dig <= 9; dig++)
+	{
+		printf("%d", dig);
+	}
+	printf('\n');
+
+	return (0);
+}
