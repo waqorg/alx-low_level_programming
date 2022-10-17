cbab74d2a17f8e154135fa6e0d2c6577cceda4f8
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-17 16:21:18 +0000

    a program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: print all possible different combinations of two digits.
+ *
+ * Return:0
+*/
+
+int main(void)
+{
+	int dig1 = 0, dig2;
+
+	while (dig1 <= 9)
+	{
+		dig = 0;
+		while (dig2 <= 9)
+		{
+			if (dig1 != dig2 && dig1 < dig2)
+			{
+				putchar(dig1 + 48);
+				putchar(dig2 + 48);
+
+				if (dig1 + dig2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++dig2;
+		}
+		++dig1;
+	}
+	putchar('\n');
+	return (0);
+}
