4dba6f8c669a823a8476dbf2c11ce263eb07b5d5
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-18 17:29:14 +0000

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+nclude"main.h"
+
+/**
+ * main - Entry
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
