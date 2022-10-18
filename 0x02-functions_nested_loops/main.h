44bceb3717f6ed2f8614bb6e5e90b039e9e25e99
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-18 17:33:54 +0000

    print _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#include "../main.h"
+/**
+ * main - Entry
+ *
+ * Description: check the code for ALX School students
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
