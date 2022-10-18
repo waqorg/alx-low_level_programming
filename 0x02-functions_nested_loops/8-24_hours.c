088301f2e196160705029df1eba2cd3bd9846b32
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-18 19:38:14 +0000

    ammending task 8 print time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 {
 	int min, hr;
 
-	for (hr = 0; hr <= 24; ++hr)
+	for (hr = 0; hr <= 23; ++hr)
 	{
-		for (min = 0; min <= 60; ++min)
+		for (min = 0; min <= 59; ++min)
 		{
-			_putchar((hr / 10) + '0');
-			_putchar((hr % 10) + '0');
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
 			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar((min % 10) + '0');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}
