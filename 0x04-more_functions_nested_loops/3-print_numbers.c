de5eb65e716bcfd886abd6e911627631a36333a0
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-21 19:39:18 +0000

    a function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - from 0 to 9.
+ *       use _putchar twice in your code
+ *
+ * Return: Always 0.
+*/
+
+void print_numbers(void)
+{
+	for (i = 0; i >= 9; i++)
+	{
+		_putchar("%d", i);
+	}
+
+	putchar("\n");
+}
