6124858ef0f593fa7346dc1eaec661ad0c2f9d53
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-21 21:16:18 +0000

    a function that prints squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,19 @@ void print_square(int size)
 {
 	int r, c;
 
-	for (r = 0; r <= size; r++)
+	if (size <= 0)
 	{
-		for (c = 0; c < size; c++)
-		{
-			_putchar('#');
-		}
-
 		_putchar('\n');
+	}
+	else
+	{
+		for (r = 0; r < size; r++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
 	}
 }
