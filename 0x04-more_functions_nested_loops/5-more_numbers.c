b4b503354ab6fa2ae28340766ae8148f93d3b8bd
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-21 20:39:49 +0000

    a function that prints the number 0-14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,22 +5,22 @@
  * Return: Always 0.
 */
 
-void more_numbers(void)
+void more_numbers(void);
 {
-	int i, j;
+	int num, row, count;
 
-	for (i = 0; i <= 10; i++)
+	for (row = 1; row <= 10; ++row)
 	{
-		for (j = 0; j <= 14; j++)
+		for (count = 0; count <= 14; ++count)
 		{
-			if (j > 9)
+			num = count;
+			if (count > 9)
 			{
-				_putchar((j / 10) + 48);
+				_putchar(1 + 48);
+				num = count % 10;
 			}
-			
-			_putchar((j % 10) + 48);
+			_putchar(num + 48);
 		}
-
 		_putchar('\n');
 	}
 }
