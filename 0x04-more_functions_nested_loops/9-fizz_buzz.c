9596f231b509730155399066729ca0c35437e471
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-21 22:33:16 +0000

    Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,14 @@ int main(void)
 {
 	int x;
 
-	for (x = 1; x <= 100; x++)
+	for (x = 1; x < 100; x++)
 	{
-		if (((x % 3) == 0) && !((x % 5) == 0))
+		if ((x % 3) == 0)
 		{
 			printf("Fizz ");
 
 		}
-		else if (((x % 5) == 0) && !((x % 3) == 0))
+		else if ((x % 5) == 0)
 		{
 			printf("Buzz ");
 
@@ -35,6 +35,7 @@ int main(void)
 		}
 	}
 
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
