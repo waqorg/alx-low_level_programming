d3b3179b80aa5c8d113b46a9145549b33ed30719
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-21 18:17:57 +0000

    how many days are left in the year, taking leap year into consideration

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,17 +8,16 @@
  * @day: day of month
  * @year: year
  * Return: void
-*/
+ * */
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 100 != 0 && year % 400 == 0) || (year % 4 == 0)
+	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
 		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
