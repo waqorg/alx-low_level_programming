75ce3dadd6452946fdba1df9e835bc50cd240ec2
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-21 20:08:06 +0000

    a function that prints the numbers, from 0 to 9 minus 2&4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_most_numbers - prints from 0 to 9.
  * minus 2 and 4
@@ -13,7 +14,13 @@ void print_most_numbers(void)
 	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 || i == 4)
-			_putchar(i + '0');
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + 48);
+		}
 	}
 	_putchar("\n");
 }
