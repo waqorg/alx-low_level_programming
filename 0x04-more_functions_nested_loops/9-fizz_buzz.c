45be9ae147e7baef4c2a335c85b6d005feb8e2d9
waqorg <25316657+waqorg@users.noreply.github.com>
2022-10-21 22:05:12 +0000

    Fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include"stdio.h"
+
+/**
+ * main - Description: fizz_buzz - prints the numbers from 1 to 100,
+ *   followed by a new line. But for multiples of three print Fizz
+ *   for the multiples of five print Buzz
+ *   multiples of both three and five print FizzBuzz
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3) == 0)
+		{
+			printf("Fizz ");
+
+		}
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz ");
+
+		}
+		else if (((x % 5) == 0) && ((x % 3) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+
+	printf("\n");
+	return (0);
+}
