5461db1ceb3dde33ee39f9e6385e8c6661251993
waqorg <25316657+waqorg@users.noreply.github.com>
2022-11-01 17:46:25 +0000

    a function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,15 +7,20 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int b, c;
+	int i, j;
 
-	for (b = 0; b < 8; b++)
+	i = 0;
+	j = 0;
+	while (i < 64)
 	{
-		for (c = 0; c < 8; c++)
+		if (i % 8 == 0 && i != 0)
 		{
-			_putchar(a[b][c]);
+			j = i;
+			_putchar('\n');
 		}
 
-		_putchar('\n');
+		_putchar(a[i / 8][i - j]);
+		i++;
 	}
+	_putchar('\n');
 }
