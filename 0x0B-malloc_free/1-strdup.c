8f5129a61eb0c87b8059dda2df560b6900033187
waqorg <25316657+waqorg@users.noreply.github.com>
2022-11-09 17:17:24 +0000

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,23 +11,24 @@
 
 char *_strdup(char *str)
 {
-	int i = 0, l = 0;
-	char *s;
+	int size = 0;
+	char *ptr, *ret;
 
-	if (str == NULL)
+	if (!str)
 		return (NULL);
 
-	/*calculate size of str*/
-	while (str[l] != '\0')
-		l++;
+	ptr = str;
+	while (*ptr++)
+		size++;
 
-	s = malloc((l + 1) * sizeof(char));
-	if (s == NULL)
+	ret = malloc(size + 1);
+	if (!ret)
 		return (NULL);
 
-	while (str[i] != '\0')
-	{
-		s[i] = str[i];
-		i++;
-	}
+	ptr = ret;
+	while (*str)
+		*ptr++ = *str++;
+
+	*ptr = 0;
+	return (ret);
 }
