f2cc548eca4afd1c0ab89fc5993bf0501a2c65b0
waqorg <25316657+waqorg@users.noreply.github.com>
2022-12-01 18:49:21 +0000

    a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -11,10 +11,8 @@ void free_listint(listint_t *head)
 {
 	listint_t *current;
 
-	/*iterate through each node in list*/
 	while ((current = head) != NULL)
 	{
-		/*set head as the next node*/
 		head = head->next;
 		free(current);
 	}
