cd889bc245ed63acd2b40001a1e889fce62e826a
waqorg <25316657+waqorg@users.noreply.github.com>
2022-12-01 19:02:38 +0000

    a function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,49 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at
- *                           a given position
- *
- * @head: pointer to the first node of the list
- * @idx: is the index of the list where the new node should be added
- * @n: element to add to the new node
- *
- * Return: NULL if anything fails or the address of the new node
-*/
+  * insert_nodeint_at_index - inserts a new node at a given position.
+  * @head: pointer to the list.
+  * @idx: position to add the node.
+  * @n: data for the new node.
+  * Return: the address of the new node, or NULL if it failed
+  **/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current;
+	listint_t *aux_node = *head;
+	listint_t *new_node;
 	unsigned int index;
+	unsigned int cont = 0;
 
-	current = *head; 
-
+	/* create node */
 	new_node = malloc(sizeof(listint_t));
-	if ((*head == NULL && idx != 0) || new_node == NULL)
+	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
 
-	new_node->n = n; 
-
-	for (index = 0; head != NULL && index < idx - 1; index++)
-	{
-		current = current->next;
-		if (current == NULL)
-			return (NULL);
-	}
-
-	if (idx == 0) 
+	/* border case for insert at the beginning */
+	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
-	}
-	else if (current->next) 
-	{
-		new_node->next = current->next; 
-		current->next = new_node;
-	}
-	else 
-	{
-		new_node->next = NULL;
-		current->next = new_node;
+		return (*head);
 	}
 
-	return (new_node);
+	/* search of position to insert */
+	index = idx - 1;
+	while (aux_node && cont != index)
+	{
+		cont++;
+		aux_node = aux_node->next;
+	}
+
+	/* general case */
+	if (cont == index && aux_node)
+	{
+		new_node->next = aux_node->next;
+		aux_node->next = new_node;
+		return (new_node);
+	}
+
+	free(new_node);
+	return (NULL);
 }
