7abe8fc8810dc892398798eae9f6882c7285c4c0
waqorg <25316657+waqorg@users.noreply.github.com>
2022-12-01 18:58:35 +0000

    a function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,15 +15,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *current;
 	unsigned int index;
 
-	current = *head; /*place first node at current*/
+	current = *head; 
 
 	new_node = malloc(sizeof(listint_t));
 	if ((*head == NULL && idx != 0) || new_node == NULL)
 		return (NULL);
 
-	new_node->n = n; /* add our element to the new node*/
+	new_node->n = n; 
 
-	/*iterate list to node position idx - 2*/
 	for (index = 0; head != NULL && index < idx - 1; index++)
 	{
 		current = current->next;
@@ -31,22 +30,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 	}
 
-	if (idx == 0) /*if the index for new node is 0*/
+	if (idx == 0) 
 	{
-		/*first node will be moved to second node*/
 		new_node->next = *head;
-		/*new node will be placed as the first node*/
 		*head = new_node;
 	}
-	else if (current->next) /*if index where to add our new node is not 0*/
+	else if (current->next) 
 	{
-		new_node->next = current->next; /*place current node after new node*/
-		current->next = new_node;/*set the new node at index idx*/
+		new_node->next = current->next; 
+		current->next = new_node;
 	}
-	else /*if node position is not present in the list*/
+	else 
 	{
-		new_node->next = NULL;/*set next addr as NULL, indicates last node*/
-		current->next = new_node;/*set the new node at the last position in list*/
+		new_node->next = NULL;
+		current->next = new_node;
 	}
 
 	return (new_node);
