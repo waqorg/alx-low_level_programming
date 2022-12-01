51f9693647933e6f8cd02fe884ee2457e6c047ec
waqorg <25316657+waqorg@users.noreply.github.com>
2022-12-01 18:43:17 +0000

    a function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,6 @@ size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 1;
 
-	/* return 0 as no of nodes when h is null*/
 	if (h == NULL)
 		return (0);
 
@@ -19,13 +18,10 @@ size_t print_listint(const listint_t *h)
 	{
 		printf("%d\n", h->n);
 
-		/*go to the next node*/
 		h = h->next;
-		/*count no of nodes*/
 		node_count++;
 	}
 
-	/*print last node*/
 	printf("%d\n", h->n);
 
 	return (node_count);
