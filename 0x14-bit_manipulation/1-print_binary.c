491812cd47e5e39fb176c8f23591406e72d349a5
waqorg <25316657+waqorg@users.noreply.github.com>
2022-12-05 18:55:11 +0000

    a function that prints the binary representation of a number.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * Description - this is a version With Debug Option, this won't work
+ * Description - this is a version with Debug Option, this won't work
  *		with the checker copy go to the readme and click
  *		checker version that one will work with the checker.
 */
