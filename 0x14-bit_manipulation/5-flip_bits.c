f46ec39192fb4a94d530963b361f70cc53566e8a
waqorg <25316657+waqorg@users.noreply.github.com>
2022-12-05 19:17:24 +0000

    edit flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,65 +1,34 @@
-#include "main.h"
 
 /**
- * print_bin - prints binary rep of @n
- *
- * @number: decimal value
- *
- * Return: nothing
-*/
-void print_bin(unsigned long int number)
-{
-	if (number >> 1)
-		print_bin(number >> 1);
+  * flip_bits - gets the number of bits flipped form n to m
+  * @n: initial lu int
+  * @m: secondary lu int
+  *
+  * Return: number of bits need to flip to get from `n to `m`
+  */
 
-	putc((number & 1) ? '1' : '0', stdout);
-}
-
-/**
- * flip_bits - a function that returns the number of bits you
- *             would need to flip to get from one number to
- *             another..
- *
- * @n: decimal number
- * @m: the other decimal number
- *
- * Return: number of bits to flip to get @m from @n
-*/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int diff_bits = 0;
-	unsigned long int diff;
+	unsigned long int size;
+	unsigned long int flipped;
+	unsigned long int i;
 
-	/*xor n and b*/
-	diff = n ^ m;
+	size = sizeof(n) * 8 - 1;
 
-	#ifdef DEBUG
-	printf("\n(n) %ld and (m) %ld in binary is ", n, m);
-	print_bin(n);
-	printf(" ");
-	print_bin(m);
-	printf(" diff is %ld and in binary is ", diff);
-	print_bin(diff);
-	printf("\n");
-	#endif
+	flipped = 0;
+	i = 0;
 
-	/*keep shifting diff to right*/
-	do {
-		#ifdef DEBUG
-		printf("diff %ld before right shift ", diff);
-		print_bin(diff);
-		printf("\n");
-		#endif
+	while (i <= size)
+	{
+		if ((n & 1) != (m & 1))
+			flipped++;
 
-		diff_bits += (diff & 1);
-		diff >>= 1;
+		n = n >> 1;
+		m = m >> 1;
 
-		#ifdef DEBUG
-		printf("diff %ld after right shift ", diff);
-		print_bin(diff);
-		printf(" and diff_bit %d before leaving loop\n\n", diff_bits);
-		#endif
-	} while (diff > 0);
+		i++;
+	}
 
-	return (diff_bits);
+	return (flipped);
+
 }
