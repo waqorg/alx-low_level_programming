8e705a7187db044f14032a1be4ca9f6bd9b8b091
waqorg <25316657+waqorg@users.noreply.github.com>
2022-12-07 18:56:05 +0000

    a program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <elf.h>
+#define BUFSIZE 1204
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
