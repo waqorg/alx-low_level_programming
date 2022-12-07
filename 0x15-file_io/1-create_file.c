f8fc87273a40e943d98061215f66369eec830a60
waqorg <25316657+waqorg@users.noreply.github.com>
2022-12-07 18:39:22 +0000

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL) /*check if filename is present*/
 		return (-1);
 
-	/*open file by creating it and if it exists write but truncate to 0*/
+	/*open file by creating it and if it exists write and truncate to 0*/
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1) /*check if file creation was a success*/
 		return (-1);
